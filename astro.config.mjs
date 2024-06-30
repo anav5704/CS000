b15e0a2680f1fe46eb41a4baa3a12ac1634f08d3
Anav Chand <anavkki@gmail.com>
2024-06-30 23:47:12 +1200

    build: initialise project

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
+
+
+// https://astro.build/config
+export default defineConfig({
+    output: 'server',
+    adapter: vercelServerless(),
+    integrations: [mdx(), tailwind(), react(), sitemap()]
+});
