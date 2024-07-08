377d302fe9d1aaf891a469e65b9daeee37470ee0
Anav Chand <anavkki@gmail.com>
2024-07-08 17:47:15 +1200

    feat: add clerk auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,29 @@
-import vercelServerless from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import clerk from 'astro-clerk-auth'
+import node from '@astrojs/node'
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://cs000.anav.dev",
     output: 'server',
-    adapter: vercelServerless(),
+    adapter: node({
+        mode: 'standalone',
+    }),
     devToolbar: {
         enabled: false
     },
-    integrations: [mdx(), tailwind(), react(), sitemap()]
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: "hover"
+    },
+    integrations: [mdx(), tailwind(), react(), sitemap(), clerk({
+        afterSignInUrl: "/",
+        afterSignUpUrl: "/",
+        signInUrl: '/auth/signin',
+        signUpUrl: '/auth/signup',
+    })],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="astro/client" />
+/// <reference types="astro-clerk-auth/env" />
