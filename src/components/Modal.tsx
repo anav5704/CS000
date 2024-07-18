c41146fc0196f48e052eec4c4bbde3cbe293af6d
Anav Chand <anavkki@gmail.com>
2024-07-18 20:20:08 +1200

    feat: add wrapper modal

---
+++ src/components/Modal.tsx
@@ -0,0 +1,30 @@
+import { X } from "lucide-react"
+
+interface Props {
+    header: string,
+    children: React.ReactNode,
+    isOpen: boolean,
+    handleClose: () => void
+}
+
+export const Modal = ({ header, children, isOpen, handleClose }: Props) => {
+    if (!isOpen) return null
+
+    return (
+        <div className="bg-black/25 backdrop-blur-xl fixed z-50 top-0 left-0 h-screen w-screen flex items-center justify-center" >
+            <dialog
+                open={isOpen}
+                className="relative bg-white rounded-2xl text-center p-7 flex flex-col items-center gap-10 w-full max-w-md"
+            >
+                <div
+                    onClick={handleClose}
+                    className="text-zinc-400 hover:text-black transition absolute top-0 right-0 cursor-pointer p-5"
+                >
+                    <X size={20} />
+                </div>
+                <h1 className="text-2xl font-bold">{header}</h1>
+                {children}
+            </dialog>
+        </div >
+    )
+}
