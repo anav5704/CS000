18f8f9662f14ecb3586b37c8140ae861155b3005
Anav Chand <anavkki@gmail.com>
2024-07-19 18:26:54 +1200

    feat(api): add update bonus progress

---
+++ src/pages/api/progress/bonus.ts
@@ -0,0 +1,62 @@
+import { getSession } from "auth-astro/server"
+import type { APIRoute } from "astro"
+import { db } from "@prisma"
+
+export const PATCH: APIRoute = async ({ request }) => {
+    const session = await getSession(request)
+    const body = await request.json()
+
+    const chapterId = body.chapterId
+    const email = session?.user?.email
+
+    if (!chapterId || !email) {
+        return new Response(
+            JSON.stringify({
+                message: "Missing required fields",
+            }),
+            { status: 400 }
+        )
+    }
+
+    const progress = await db.progress.findFirst({
+        where: {
+            user: {
+                email
+            }
+        }
+    })
+
+    if (progress) {
+        if (progress.soloCompleted.includes(chapterId)) {
+            await db.progress.update({
+                where: {
+                    id: progress.id
+                },
+                data: {
+                    soloCompleted: {
+                        set: progress.bonusCompleted.filter((id) => id !== chapterId),
+                    }
+                }
+            })
+        }
+        else {
+            await db.progress.update({
+                where: {
+                    id: progress.id
+                },
+                data: {
+                    bonusCompleted: {
+                        push: chapterId
+                    }
+                }
+            })
+        }
+    }
+
+    return new Response(
+        JSON.stringify({
+            message: "Success!"
+        }),
+        { status: 200 }
+    )
+}
