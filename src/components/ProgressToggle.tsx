a924f9b7620a05463a2b3143b0abfc3f8eb28665
Anav Chand <anavkki@gmail.com>
2024-07-19 18:24:45 +1200

    feat: add mark as completed button

---
+++ src/components/ProgressToggle.tsx
@@ -0,0 +1,52 @@
+import { progress, removeItem, addItem } from "@context/ProgressStore"
+import { CheckBox } from "@components/CheckBox"
+import { useState } from "react"
+
+interface Props {
+    lesson: "Solo" | "Team" | "Pro" | "Bonus"
+    chapterId: number
+}
+
+
+export const ProgressToggle = ({ lesson, chapterId }: Props) => {
+    const [isLoading, setIsLoading] = useState(false)
+
+    const handleClick = async () => {
+        try {
+            setIsLoading(true)
+
+            await fetch("/api/progress/" + lesson.toLowerCase(), {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    chapterId,
+                })
+            })
+
+            const key = lesson.toLowerCase() + 'Completed' as "soloCompleted" | "teamCompleted" | "proCompleted" | "bonusCompleted"
+
+            const isChecked = progress.get()[key].includes(chapterId)
+
+            if (isChecked) {
+                removeItem(key, chapterId)
+            } else {
+                addItem(key, chapterId)
+            }
+
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+
+    return (
+        <button onClick={handleClick} disabled={isLoading} className="transition disabled:cursor-not-allowed disabled:opacity-50 flex items-center gap-3 mt-10">
+            <CheckBox lesson={lesson} chapterId={chapterId} isDisabled={isLoading} />
+            <p>{isLoading ? "One sec, updating..." : "Mark as completed"}</p>
+        </button>
+    )
+}
