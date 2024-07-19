987008ff70b104b3cbd5df5d2db7d38e76828b49
Anav Chand <anavkki@gmail.com>
2024-07-19 18:26:27 +1200

    feat: add middleware for auth

---
+++ src/middleware/index.ts
@@ -0,0 +1,11 @@
+import { defineMiddleware } from "astro:middleware"
+import { getSession } from "auth-astro/server"
+
+export const onRequest = defineMiddleware(async (context, next) => {
+    const session = await getSession(context.request)
+
+    if (session?.user) context.locals.user = session.user
+
+    return next()
+
+})
