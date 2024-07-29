b1e87e5826791539c6f8e551684ca0848fb9c680
Anav Chand <anavkki@gmail.com>
2024-07-29 20:54:38 +1200

    chore: revert to old middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,41 +1,16 @@
-import { defineMiddleware, sequence } from "astro:middleware"
+import { defineMiddleware } from "astro:middleware"
 import { getSession } from "auth-astro/server"
 
-const handleAuth = defineMiddleware(async (context, next) => {
+export const onRequest = defineMiddleware(async (context, next) => {
     const session = await getSession(context.request)
 
-    if (session?.user) context.locals.user = session.user as {
-        email: string
-        name: string
-        image?: string
-    }
-
-    const response = await next()
-    return response
-})
-
-const handleRedirects = defineMiddleware(async (context, next) => {
-    const session = await getSession(context.request)
-
-    const protectedRoutes = [
-        "/lessons/solo",
-        "/lessons/team",
-        "/lessons/pro",
-        "/lessons/bonus"
-    ]
-
-    if (protectedRoutes.some((route) => context.url.pathname.includes(route))) {
-        if (!session?.user) {
-            console.log("redirecting")
-            return new Response(null, {
-                status: 302,
-                headers: { Location: "/lessons?login=true" },
-            })
+    if (session?.user) {
+        context.locals.user = session.user as {
+            email: string
+            name: string
+            image?: string
         }
     }
 
-    const response = await next()
-    return response
+    return next()
 })
-
-export const onRequest = sequence(handleAuth)
